86b1eb374f38e7a140e563e2dba1d264224afd52
Mark Muhleder <muhleder@users.noreply.github.com>
2020-12-26 13:22:33 +0000

    Detect FLUTTER_WEB_USE_SKIA

--- lib/src/websafe_svg.dart
+++ lib/src/websafe_svg.dart
@@ -2,12 +2,17 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 
+import 'platform/io_svg.dart' as io_svg;
+
 // ignore: uri_does_not_exist
 import 'platform/stub_svg.dart'
     // ignore: uri_does_not_exist
     if (dart.library.io) 'platform/io_svg.dart'
     // ignore: uri_does_not_exist
-    if (dart.library.html) 'platform/browser_svg.dart';
+    if (dart.library.html) 'platform/browser_svg.dart' as universal_svg;
+
+const bool _useSkia =
+bool.fromEnvironment('FLUTTER_WEB_USE_SKIA', defaultValue: false);
 
 class WebsafeSvg {
   static Widget asset(
@@ -21,7 +26,19 @@ class WebsafeSvg {
     String semanticsLabel,
     double width,
   }) =>
-      svgFromAsset(
+      _useSkia ?
+      io_svg.svgFromAsset(
+        asset,
+        alignment: alignment,
+        color: color,
+        fit: fit,
+        height: height,
+        package: package,
+        placeholderBuilder: placeholderBuilder,
+        semanticsLabel: semanticsLabel,
+        width: width,
+      ) :
+      universal_svg.svgFromAsset(
         asset,
         alignment: alignment,
         color: color,
@@ -43,7 +60,18 @@ class WebsafeSvg {
     String semanticsLabel,
     double width,
   }) =>
-      svgFromMemory(
+      _useSkia ?
+      io_svg.svgFromMemory(
+        bytes,
+        alignment: alignment,
+        color: color,
+        fit: fit,
+        height: height,
+        placeholderBuilder: placeholderBuilder,
+        semanticsLabel: semanticsLabel,
+        width: width,
+      ) :
+      universal_svg.svgFromMemory(
         bytes,
         alignment: alignment,
         color: color,
@@ -65,7 +93,19 @@ class WebsafeSvg {
     String semanticsLabel,
     double width,
   }) =>
-      svgFromNetwork(
+      _useSkia ?
+      io_svg.svgFromNetwork(
+        url,
+        alignment: alignment,
+        color: color,
+        fit: fit,
+        headers: headers,
+        height: height,
+        placeholderBuilder: placeholderBuilder,
+        semanticsLabel: semanticsLabel,
+        width: width,
+      ) :
+      universal_svg.svgFromNetwork(
         url,
         alignment: alignment,
         color: color,
@@ -87,7 +127,18 @@ class WebsafeSvg {
     String semanticsLabel,
     double width,
   }) =>
-      svgFromString(
+      _useSkia ?
+      io_svg.svgFromString(
+        bytes,
+        alignment: alignment,
+        color: color,
+        fit: fit,
+        height: height,
+        placeholderBuilder: placeholderBuilder,
+        semanticsLabel: semanticsLabel,
+        width: width,
+      ) :
+      universal_svg.svgFromString(
         bytes,
         alignment: alignment,
         color: color,
